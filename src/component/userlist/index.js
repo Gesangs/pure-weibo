b33cfa6c364d122462f3f0959e83416affffe6d2
gesangs <gesangs@qq.com>
2018-02-10 23:31:58 +0800

    userlist

--- src/component/userlist/index.js
+++ src/component/userlist/index.js
@@ -6,13 +6,19 @@ class UserList extends Component{
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
         this.state = {
-            
+            userlist: []
         }
     }
     render(){
+        const { userlist } = this.state;
+        const { title } = this.props; 
         return(
             <div>
-                {<div className="listHead">
+                <div className="userlist-title">
+                    <span>返回</span> 
+                    { title }
+                </div>
+                {userlist.map((item, index) => <div className="listHead" key={index}>
                     <img alt="head" src={user.head_pic} className="listPic" onClick={this.goToUser.bind(this, user)} />
                     <div className="listNameS">
                         <span className="listName" onClick={this.goToUser.bind(this, weibo.user)}>{user.name}</span>
@@ -20,7 +26,7 @@ class UserList extends Component{
                             {user.description}
                         </div>
                     </div>
-                </div>}
+                </div>)}
             </div>
         )
     }
