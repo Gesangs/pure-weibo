07259ab7f888fc910e9e13431e3d02fc465ac96f
gesangs <gesangs@qq.com>
2018-02-18 23:22:14 +0800

    fix

--- src/component/userpage/aboutuser/index.js
+++ src/component/userpage/aboutuser/index.js
@@ -9,20 +9,21 @@ class AboutUser extends Component {
   render() {
     const userinfo = this.props.userinfo;
     const list = [
-      { lable: "签名", content: userinfo.description || "无" },
-      { lable: "微博认证", content: userinfo.verified_reason || "无" },
-      { lable: "所在地", content: userinfo.location || "无" },
-      { lable: "性别", content: userinfo.gender || "无" },
-      { lable: "注册时间", content: userinfo.create_time || "无" }
+      { lable: "签名", content: userinfo.description },
+      { lable: "微博认证", content: userinfo.verified_reason },
+      { lable: "所在地", content: userinfo.location },
+      { lable: "性别", content: userinfo.gender },
+      { lable: "注册时间", content: userinfo.create_time }
     ];
     return (
       <React.Fragment>
-        {list.map((item, index) => (
-          <div className="userinfo" key={index}>
+        {list.map((item, index) => {
+          return item.content 
+            ? <div className="userinfo" key={index}>
             <span className="infoLable">{item.lable}</span>
             <p className="infoContent">{item.content}</p>
-          </div>
-        ))}
+          </div> : ""
+      })}
       </React.Fragment>
     );
   }
