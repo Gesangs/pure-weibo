e5941a8f046b97fa7867642d7b1ff2c5574dd689
gesangs <gesangs@qq.com>
2018-04-18 22:49:50 +0800

    fix loadmore

--- src/component/loadmore/index.js
+++ src/component/loadmore/index.js
@@ -7,14 +7,15 @@ class LoadMore extends React.Component {
     constructor(props, context) {
         super(props, context);
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.load = null
     }
     render() {
         return (
-            <div className="load-more" ref="wrapper">
+            <div className="load-more" ref={(load) => this.load = load}>
                 {
                     this.props.isLoadingMore
                     ? <span>加载中...</span>
-                    : <span onClick={this.loadMoreHandle}>加载更多</span>
+                    : <span onClick={this.loadMoreHandle}>没有了~</span>
                 }
             </div>
         )
@@ -25,26 +26,23 @@ class LoadMore extends React.Component {
     }
     componentDidMount() {
         // 使用滚动时自动加载更多
-        const loadMoreFn = this.props.loadMoreFn
-        const wrapper = this.refs.wrapper
         let timeoutId
-        function callback() {
-            const top = wrapper.getBoundingClientRect().top
+        const loadMoreFn = this.props.loadMoreFn
+        const callback = () => {
+            const top = this.load.getBoundingClientRect().top
             const windowHeight = window.screen.height
             if (top && top < windowHeight) {
                 // 证明 wrapper 已经被滚动到暴露在页面可视范围之内了
                 loadMoreFn()
             }
         }
-        window.addEventListener('scroll', function () {
-            if (this.props.isLoadingMore) {
-                return
-            }
+        window.addEventListener('scroll', () => {
+            if (!this.props.isLoadingMore) return
             if (timeoutId) {
                 clearTimeout(timeoutId)
             }
             timeoutId = setTimeout(callback, 50)
-        }.bind(this), false);
+        }, false);
     }
 }
 
